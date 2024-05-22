eab11a09f07968708ed274da9c0c808e12f92172
Via Skillings <viaskillings@gmail.com>
2024-05-22 17:29:58 -0500

    Updated answers

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
 
-const questions = [
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter text for the logo (up to 3 characters):',
-      validate: input => input.length <= 3 || 'Text must be 3 characters or less',
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter the text color (keyword or hexadecimal):',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Choose a shape for the logo:',
-      choices: ['circle', 'triangle', 'square'],
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter the shape color (keyword or hexadecimal):',
-    },
-  ];
+const fs = require("fs")
+const { Triangle, Square, Circle } = require("./lib/shapes");
+
+function writeToFile(fileName, answers) {
+    let svgString = "";
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgString += "<g>";
+    svgString += `${answers.shape}`;
+
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+        shapeChoice = new Triangle();
+        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "Square") {
+        shapeChoice = new Square();
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    } else {
+        shapeChoice = new Circle();
+        svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    }
+
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    svgString += "</g>";
+    svgString += "</svg>";
+
+    fs.writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg");
+    })
+}
